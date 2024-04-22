c844664485cb36db9e57c7f0ad32b51677e0a071
teatime77 <teatime77@live.jp>
2024-04-22 12:08:43 +0900

    マップのスケーリングの途中

--- ts/map.ts
+++ ts/map.ts
@@ -1,13 +1,36 @@
 namespace casts {
 //
-const scale = 0.2;
+const scale = 1;
 const margin = scale * 20;
 const textMargin = scale * 5;
 const strokeWidth = scale * 1;
-const fontSize = scale * 10;
+const fontSize = scale * 16;
 const rowWidthLimit = scale * 500;
 
 const borderWidth = 3;
+
+function F(x : number) : string {
+    return x.toFixed();
+}
+
+enum Display {
+    hide,
+    title,
+    partial,
+    full,
+    unknown
+}
+
+function displayText(display : Display) : string {
+    switch(display){
+    case Display.hide    : return "hide";
+    case Display.title   : return "title";
+    case Display.partial : return "partial";
+    case Display.full    : return "full";
+    case Display.unknown : return "unknown";
+    }
+}
+
 
 export class MapSVG {
     svg : SVGSVGElement;
@@ -88,75 +111,126 @@ export class MapSVG {
     }
 
     onWheel(ev: WheelEvent){
-        const border = 3;
-
-        const offsetX = ev.offsetX;
-        const offsetY = ev.offsetY;
-        msg("");
-        msg(`offset:${offsetX.toFixed()} ${offsetY.toFixed()} min:${this.minX.toFixed()} ${this.minY.toFixed()} scale:${(100 * this.scale).toFixed(1)} view:${this.viewWidth} ${this.viewHeight}`);
-
-        const bnd = this.svg.getBoundingClientRect();
-        const bnd_x = 0.5 * (this.viewWidth  + 6) * (1.0 - this.scale);
-        const bnd_y = 0.5 * (this.viewHeight + 6) * (1.0 - this.scale);
-        msg(`bound:${bnd.x.toFixed()} ${bnd.y.toFixed()} ${bnd.width.toFixed()} ${bnd.height.toFixed()} bound-diff:${(bnd_x - bnd.x).toFixed()} ${(bnd_y - bnd.y).toFixed()}`);
-
-        const cli_x = bnd.x + (border + offsetX) * this.scale;
-        const cli_y = bnd.y + (border + offsetY) * this.scale;
-        const cli_x_diff = (cli_x - ev.clientX).toFixed();
-        const cli_y_diff = (cli_y - ev.clientY).toFixed();
-        msg(`client:${ev.clientX.toFixed()} ${ev.clientY.toFixed()} client-diff:${cli_x_diff} ${cli_y_diff}`);
-
-        // client = bound + (border + offset) * scale
-        // offset = (client - bound) / scale - border
-        // bound = 0.5 * (view + 2 * border) * (1 - scale)
-
         if(0 < ev.deltaY){
-            this.scale *= 1.05;
+            this.scale *= 1.01;
         }
         else{
-            this.scale /= 1.05;
+            this.scale /= 1.01;
         }
 
-        const Cx = offsetX - this.minX;
-        const Cy = offsetY - this.minY;
-
-        const [bnd_x2, bnd_y2] = this.BoundingFromScale(this.scale);
-
-        const offset_x2 = (ev.clientX - bnd_x2) / this.scale - border;
-        const offset_y2 = (ev.clientY - bnd_y2) / this.scale - border;
-
-        this.minX = offset_x2 - Cx;
-        this.minY = offset_y2 - Cy;
-        
-        this.svg.setAttribute("transform", `scale(${this.scale}, ${this.scale})`);
-
-        this.root.svg.setAttribute("x", `${this.minX}`);
-        this.root.svg.setAttribute("y", `${this.minY}`);
-
-        msg(`bnd2:${bnd_x2.toFixed()} ${bnd_y2.toFixed()} offset2:${offset_x2.toFixed()} ${offset_y2.toFixed()}`)
+        this.update();
     }
+
+    update(){
+        this.root.clearSize();
+        this.root.getWidth();
+        this.root.getHeight();
+        this.root.setAppearance();
+        this.root.setXY(this.minX, this.minY);
+        this.root.dmp("");
+    }
+
 }
 
+let itemId = 0;
+
 abstract class MapItem {
+    id : number;
+    parent! : Region | MapSVG;
+    display! : Display;
+    ratio   : number = NaN;
+    level! : number;
     title! : string;
 
     text! : SVGTextElement;
     rect! : SVGRectElement;
+    visible : boolean = false;
     textBox! : DOMRect;
-    left  : number = 0;
-    top   : number = 0;
-    width!  : number;
-    height! : number;
+
+    left   : number = NaN;
+    top    : number = NaN;
+    width  : number = NaN;
+    height : number = NaN;
+
+    abstract getWidth() : number;
+    abstract getHeight() : number;
+    abstract setXY(x : number, y : number) : void;
+
+    constructor(){
+        this.id = itemId++;
+    }
+
+    getDisplay() : void {
+        let ret : [Display, number];
+
+        const depth = this.level - mapSVG.scale;
+        if(3 < depth){
+            ret = [Display.hide, 0];
+        }
+        else if(1 < depth){
+            // 1 < depth <= 3
+            const r = 0.5 * (depth - 1);
+            ret = [Display.title, 1 - r];
+        }
+        else{
+            if(this instanceof TextMap){
+
+                ret = [Display.full, 1];
+            }
+            else{
+
+                if(0 < depth){
+                    // 0 <= depth <= 1
+                    ret = [Display.partial, 1 - depth];
+                }
+                else{
+                    ret = [Display.full, 1];
+                }
+            }
+        }
+        assert(0 <= ret[1] && ret[1] <= 1);
+
+        if(ret[1] == 0){
+
+            this.visible = false;
+        }
+        else{
+
+            this.visible = (this.parent instanceof MapSVG || this.parent.visible);
+        }
+
+        [this.display, this.ratio] = ret;
+    }
+
+    titleRatio() : number {
+        assert(this.visible);
+        if(this.display == Display.partial || this.display == Display.full){
+            return 1;
+        }
+        else{
+            return this.ratio;
+        }
+    }
+
+    titleWidth() : number {
+        assert(this.visible);
+        return this.titleRatio() * (textMargin + this.textBox.width + textMargin);
+    }
+
+    titleHeight() : number {
+        assert(this.visible);
+        return this.titleRatio() * (textMargin + this.textBox.height + textMargin);
+    }
 
     init(parent : Region | MapSVG, obj : any){
+        this.parent = parent;
         this.title = obj["title"];
 
         this.text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-        this.text.setAttribute("x", `10000`);
-        this.text.setAttribute("y", `10000`);
         this.text.setAttribute("stroke", "black");
         this.text.setAttribute("stroke-width", `${strokeWidth}`);
-        this.text.setAttribute("font-size", `${fontSize}`);
+        this.text.setAttribute("font-size", `${fontSize}pt`);
+        this.text.setAttribute("font-family", "serif");
         this.text.textContent = this.title;
 
         this.rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
@@ -164,6 +238,18 @@ abstract class MapItem {
         this.rect.setAttribute("stroke", "black");
         this.rect.setAttribute("stroke-width", `${strokeWidth}`);
         // this.rect.setAttribute("fill-opacity", `0`);      
+        this.rect.setAttribute("visibility", "hidden");
+        this.rect.setAttribute("width" , `10000`);
+        this.rect.setAttribute("height", `10000`);
+
+        if(parent instanceof MapSVG){
+
+            this.level = 1;
+        }
+        else{
+
+            this.level = parent.level + 1;
+        }
 
         if(this instanceof Region){
 
@@ -177,42 +263,106 @@ abstract class MapItem {
         }
 
         this.textBox = this.text.getBBox();
-
-        this.setWH(this.textBox.width, this.textBox.height);
     }
 
-    calcSize(){        
+    setAppearance(){
+        if(this.visible){
+            assert(this.ratio != 0 && !isNaN(this.width) && !isNaN(this.height));
+
+            if(this.display == Display.title){
+
+                this.text.setAttribute("font-size", `${this.ratio * fontSize}pt`);
+            }
+            else{
+
+                this.text.setAttribute("font-size", `${fontSize}pt`);
+            }
+            this.rect.setAttribute("width" , `${this.width}`);
+            this.rect.setAttribute("height", `${this.height}`);
+
+            if(this instanceof Region){
+                if(this.display == Display.partial || this.display == Display.full){
+
+                    this.children.forEach(x => x.setAppearance());
+                }
+            }
+        }
+
+        const value = this.visible ? "visible" : "hidden";
+
+        this.text.setAttribute("visibility", value);
+        this.rect.setAttribute("visibility", value);
+    
+        if(this instanceof Region){
+
+            if(this.display == Display.partial || this.display == Display.full){
+
+                this.svg.setAttribute("visibility", value);
+            }
+        }
     }
 
-    setXY(x : number, y : number){
-        this.left = x;
-        this.top  = y;
+    clearSize(){
+        this.getDisplay();
+        this.left   = NaN;
+        this.top    = NaN;
+        this.width  = NaN;
+        this.height = NaN;
 
-        this.text.setAttribute("x", `${textMargin + x}`);
-        this.text.setAttribute("y", `${y + this.textBox.height}`);
+        this.text.setAttribute("visibility", "hidden");
+        this.rect.setAttribute("visibility", "hidden");
+
+        if(this instanceof Region){
+            this.svg.setAttribute("visibility", "hidden");
+
+            this.children.forEach(x => x.clearSize());
+
+            if(this.display == Display.partial || this.display == Display.full){
+
+                this.visibleRows = this.rows.map(row => row.filter(item => item.visible));
+                this.visibleRows = this.visibleRows.filter(row => row.length != 0);
+            }
+            else{
+                this.visibleRows = [];
+            }
+        }
+    }
+
+    margin() : number {
+        return this.ratio * margin;
+    }
+
+    setXYAttribute(x : number, y : number){
+        const title_ratio = this.titleRatio();
+
+        this.text.setAttribute("x", `${x + title_ratio * textMargin}`);
+        this.text.setAttribute("y", `${y + title_ratio * (textMargin + this.textBox.height)}`);
 
         this.rect.setAttribute("x", `${x}`);
         this.rect.setAttribute("y", `${y}`);
     }
 
-    setWH(width : number, height : number){        
-        if(this instanceof TextMap){
-
-            this.width  = textMargin + width  + textMargin;
-            this.height = textMargin + height + textMargin;
-        }
-        else{
-
-            this.width = width;
-            this.height = height;
-        }
-
-        this.rect.setAttribute("width" , `${this.width}`);
-        this.rect.setAttribute("height", `${this.height}`);
-    }
-
     dmp(nest : string){
-        msg(`${nest}item ${this.left.toFixed()} ${this.top.toFixed()} ${this.title}`);
+        if(nest == ""){
+            msg(`scale:${mapSVG.scale.toFixed(2)} --------------------------------------------------`);
+        }
+        assert(this.visible && !isNaN(this.width) && !isNaN(this.height));
+        const t = (this instanceof Region ? "reg" : "txt");
+        const x = this.left.toFixed();
+        const y = this.top.toFixed();
+        const w = this.width.toFixed();
+        const h = this.height.toFixed();
+        msg(`${nest}${t} lvl:${this.level} id:${this.id} xywh:${x} ${y} ${w} ${h} ${this.title} ${displayText(this.display)} ${this.ratio.toFixed(2)}`);
+
+        if(this instanceof Region){
+            nest += "    ";
+
+            for(const row of this.visibleRows){
+                for(const item of row){
+                    item.dmp(nest);
+                }
+            }
+        }
     }
 }
 
@@ -221,11 +371,46 @@ class TextMap extends MapItem {
         super();
         this.init(parent, obj);
     }
+
+    getWidth() : number {
+        assert(this.visible);
+
+        if(isNaN(this.width)){
+
+            this.width = this.titleWidth();
+        }
+
+        assert(!isNaN(this.width));
+        return this.width;
+    }
+
+    getHeight() : number {
+        assert(this.visible);
+
+        if(isNaN(this.height)){
+
+            this.height = this.titleHeight();
+        }
+
+        assert(!isNaN(this.height));
+        return this.height;
+    }
+
+    setXY(x : number, y : number) : void {
+        assert(this.visible);
+        this.left = x;
+        this.top  = y;
+
+        this.setXYAttribute(x, y);
+    }
+
 }
 
 class Region extends MapItem {
     svg : SVGSVGElement;
     children : MapItem[];
+    rows : MapItem[][] = [];
+    visibleRows! : MapItem[][];
 
     constructor(parent : Region | MapSVG, obj : any){
         super();
@@ -234,65 +419,114 @@ class Region extends MapItem {
         this.init(parent, obj);
 
         this.children = Array.from(obj["children"]).map(x => makeMap(this, x));
+
+        for(let i = 0; i < this.children.length; i += 3){
+            this.rows.push(this.children.slice(i, i + 3));
+        }
     }
 
-    calcSize(){
-        this.children.forEach(x => x.calcSize());
+    getWidth() : number {
+        assert(this.visible);
 
-        let row_top = margin + this.textBox.height + margin;
+        if(isNaN(this.width)){
 
-        let max_row_width = margin + this.textBox.width + margin;
+            let title_width = this.titleWidth();
 
-        let pending = this.children.slice();
-        while(pending.length != 0){
+            switch(this.display){
+            case Display.title:
+                this.width = title_width;
+                break;
 
-            let row : MapItem[] = [];
-            let row_left = margin;
-            while(pending.length != 0){
-                const item = pending.shift()!;
+            case Display.partial:
+            case Display.full:
+                if(this.visibleRows.length == 0){
 
-                if(row.length != 0 && rowWidthLimit < row_left + item.width + margin){
-                    pending.unshift(item);
-                    break;
+                    this.width = title_width;
                 }
+                else{
 
-                item.setXY(row_left, row_top);
+                    let row_widths = this.visibleRows.map(row => sum(row.map(item => item.getWidth())) + margin * this.ratio * (row.length + 1));
+                    row_widths.unshift(title_width);
 
-                row_left += item.width + margin;
-                row.push(item);
+                    this.width = Math.max(... row_widths);
+                }
+                break;
+
+            default:
+                assert(false);
+                break;
             }
-
-            const row_width = sum(row.map(x => x.width)) + margin * (row.length + 1);
-
-            if(max_row_width < row_width){
-
-                max_row_width = row_width;
-            }
-
-            const row_height = Math.max( ... row.map(x => x.height) );
-            row_top += row_height + margin;
         }
 
-        this.setWH(max_row_width, row_top);
+        assert(!isNaN(this.width));
+        return this.width;
     }
 
-    setXY(x : number, y : number){        
+    getHeight() : number {
+        assert(this.visible);
+
+        if(isNaN(this.height)){
+
+            let title_height = this.titleHeight();
+
+            switch(this.display){
+            case Display.title:
+                this.height = title_height;
+                break;
+
+            case Display.partial:
+            case Display.full:
+                if(this.visibleRows.length == 0){
+
+                    this.height = title_height;
+                }
+                else{
+
+                    const row_heights = this.visibleRows.map(row => Math.max(... row.map(item => item.getHeight())));
+                    const row_heights_sum = sum(row_heights);
+                    this.height = title_height + row_heights_sum + this.ratio * margin * (this.visibleRows.length + 1);
+                }
+                break;
+
+            default:
+                assert(false);
+                break;
+            }
+        }
+
+        assert(!isNaN(this.height));
+        return this.height;
+    }
+
+    setXY(x : number, y : number) : void {
+        assert(this.visible);
         this.left = x;
         this.top  = y;
 
         this.svg.setAttribute("x", `${x}`);
         this.svg.setAttribute("y", `${y}`);
 
-        this.text.setAttribute("x", `${margin}`);
-        this.text.setAttribute("y", `${margin}`);
-    }
+        assert(this.ratio != 0);
+        const r_margin = this.ratio * margin;
 
-    dmp(nest : string){
-        msg(`${nest}group ${this.left.toFixed()} ${this.top.toFixed()} ${this.title}`);
+        this.setXYAttribute(0, 0);
 
-        nest += "    ";
-        this.children.forEach(x => x.dmp(nest));
+        if(this.display == Display.partial || this.display == Display.full){
 
+            let offset_y = this.titleHeight() + r_margin;
+
+            for(const row of this.visibleRows){
+                let offset_x = r_margin;
+                for(const item of row){
+                    item.setXY(offset_x, offset_y);
+
+                    offset_x += item.width + r_margin;
+                }
+
+                const max_height = Math.max(... row.map(item => item.height));
+                offset_y += max_height + r_margin;
+            }
+        }
     }
 }
 
@@ -310,11 +544,7 @@ let mapSVG : MapSVG;
 export async function bodyOnLoadMap(){
     const map = await fetchJson(`../data/map.json`);
     mapSVG = new MapSVG(map);
-
-    mapSVG.root.calcSize();
-    mapSVG.root.setXY(0, 0);
-
-    mapSVG.root.dmp("");
+    mapSVG.update();
 
     setMapEventListener(mapSVG);
 }
